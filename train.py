27c297c2f1017abee29022ce29153f6cc51f93fb
anion <1005128408@qq.com>
2024-07-27 12:20:01 +0800

    update train.py

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
+# import torch
+# import torch_npu
+# import torchvision
+# import torchvision_npu
+# from torch_npu.contrib import transfer_to_npu
 from ultralytics import YOLOv10
 
 # Load a model
 model = YOLOv10("weights/yolov10s.pt")  # load a pretrained model (recommended for training)
 
-# Train the model with 2 GPUs
-results = model.train(data="pcb-defect-dataset/data.yaml", epochs=100, imgsz=640, workers=32, device=0, plots=True, batch=240)
+# since we have early stop in the training, we can train the model for a large number of epochs
+results = model.train(data="pcb-defect-dataset/data.yaml", epochs=10000, imgsz=640, workers=32, device=0, plots=True, batch=64)
 
-print(result)
+
